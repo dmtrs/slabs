1d5531c26c2c3f7f642260cc79f6038dadf08d30
Dimitrios Meggidis <dmtrs@dmtrs.evresis>
2010-10-02 01:29:25 +0300

    mainconfig app title

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'service.lab.system',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
