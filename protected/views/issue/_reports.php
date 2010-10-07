9b9609882c51f50481ddae0d92068bf2b7f1b3c3
Dimitrios Meggidis <tydeas.dr@gmail.com>
2010-10-08 00:42:59 +0300

    changed id to html tags _report.php

--- protected/views/issue/_reports.php
+++ protected/views/issue/_reports.php
@@ -1,20 +1,20 @@
 <?php foreach($reports as $report): ?>
-<div class="comment" id="c<?php echo $report->rp_id; ?>">
+<div class="report" id="c<?php echo $report->rp_id; ?>">
 
         <?php //echo CHtml::link("#{$report->rp_id}", $report->getUrl($issue), array(
                 //'class'=>'cid',
                 //'title'=>'Permalink to this comment',
         //)); ?>
 
-        <div class="author">
+        <div class="rp_user">
                 <?php echo $report->us->us_fullname; ?> reported:
         </div>
 
-        <div class="time">
+        <div class="rp_date">
                 <?php echo $report->rp_date; ?>
         </div>
 
-        <div class="content">
+        <div class="rp_report">
                 <?php echo nl2br(CHtml::encode($report->rp_report)); ?>
         </div>
 
